ebfd1884a047c3bb835e821bd42d55f900730bc2
StarCoder576 <157668972+StarCoder576@users.noreply.github.com>
2024-04-25 22:39:20 +0000

    update script.js and style.css

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 
 
 
+function AIResponse() {
+    var inputElement = document.getElementById('QuestionInput');
+    var displayArea = document.getElementById('displayArea');
+    displayArea.innerHTML += inputElement.value + '<br>';
+    inputElement.value = ''; // Clear the input box after adding text
+}
+
+
+
 
