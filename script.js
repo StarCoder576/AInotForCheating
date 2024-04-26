a418b81888f43659e1654722f28acf026562e3ac
StarCoder576 <157668972+StarCoder576@users.noreply.github.com>
2024-04-26 01:08:39 +0000

    update script.js

--- script.js
+++ script.js
@@ -15,11 +15,7 @@ function AIResponse() {
 
 
 
-const {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } = require("@google/generative-ai");
+
   
   const MODEL_NAME = "gemini-1.0-pro";
   const API_KEY = "AIzaSyCJ26X7BEoWyiWpq0yvca58mylKZxPqbRI";
